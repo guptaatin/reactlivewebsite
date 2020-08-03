344e944a61dbb7691b2e49ed7ad22ec71fdda4ee
guptaatin <guptaatin@users.github.com>
2020-08-03 18:52:30 +0530

    Updates

---
+++ precache-manifest.2e8b53590068be82113e3a80f334cca5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716c15ab050acc68653b74fb697718b1",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "ab18e1f3800a17b69b12",
+    "url": "/reactlivewebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8f0fd1cad6cb4b50c695",
+    "url": "/reactlivewebsite/static/css/main.a0cf9ac1.chunk.css"
+  },
+  {
+    "revision": "ab18e1f3800a17b69b12",
+    "url": "/reactlivewebsite/static/js/2.0bfbd0ff.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.0bfbd0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0fd1cad6cb4b50c695",
+    "url": "/reactlivewebsite/static/js/main.6b077d4d.chunk.js"
+  },
+  {
+    "revision": "d0f68cc663c16990ee86",
+    "url": "/reactlivewebsite/static/js/runtime-main.7285ad41.js"
+  },
+  {
+    "revision": "645f061822725d4256235a69ba5769aa",
+    "url": "/reactlivewebsite/static/media/android development.645f0618.jpg"
+  },
+  {
+    "revision": "ffff3c9e54ee1db8bc6ce54ed45774f1",
+    "url": "/reactlivewebsite/static/media/app development.ffff3c9e.jpg"
+  },
+  {
+    "revision": "32148bbb729740121306967e2c79e1a7",
+    "url": "/reactlivewebsite/static/media/img1.32148bbb.jpg"
+  },
+  {
+    "revision": "fd69ed08304dc6db775bd23a00102740",
+    "url": "/reactlivewebsite/static/media/img2.fd69ed08.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactlivewebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e20d3c5728c2d61a17c69104f01c6e8",
+    "url": "/reactlivewebsite/static/media/web development.6e20d3c5.jpg"
+  }
+]);
